19ca5cb4e2d4df5f5ea843e7fbae2fe28a389c70
justwowyeah <3324009638@qq.com>
2022-08-03 22:35:32 +0800

    my_strlen1

---
+++ my_strlen1/my_strlen1/my_strlen1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int my_strlen(char* str)
+{
+	int count = 0;
+
+	while ('\0' != *str)
+	{
+		count++;
+		str++;
+	}
+
+	return count;
+}
+
+int main(void)
+{
+	int len = my_strlen("abc");
+	printf("%d", len);
+	return 0;
+}
